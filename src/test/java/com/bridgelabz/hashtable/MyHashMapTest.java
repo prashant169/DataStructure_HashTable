9e4a8a6b6f7a190c41054b9ea5a76e028c811902
prashant169 <prashantphad2018@gmail.com>
2021-10-09 02:11:43 -0800

    Program For A Frequency Of Word In Larg Paragraph

--- src/test/java/com/bridgelabz/hashtable/MyHashMapTest.java
+++ src/test/java/com/bridgelabz/hashtable/MyHashMapTest.java
@@ -1,10 +1,10 @@
 package com.bridgelabz.hashtable;
+  
 
 import junit.framework.TestCase;
 import org.junit.Test;
 
 public class MyHashMapTest {
-
 	// testCase to Find the Frequency of given words are true
 	@Test
 	public void givenASentence_WhenWordsAreAddedToList_ShouldReturnWordFrequency() {
@@ -22,5 +22,24 @@ public class MyHashMapTest {
 		int frequency = myHashMap.get("to");
 		System.out.println(myHashMap);
 		TestCase.assertEquals(2, frequency);
+	}
+
+	@Test
+	public void givenASentence_whenWordsAreAddedToList_shouldReturnParanoidWordFrequency() {
+		String sentence = "Paranoids are not paranoid because they are paranoid but "
+				+ "because they keep putting themselves deliberately " + "into paranoid avoidable situations";
+		MyHashMapParanoid<String, Integer> myLinkedHashMap = new MyHashMapParanoid<>();
+		String[] words = sentence.toLowerCase().split(" ");
+		for (String word : words) {
+			Integer value = myLinkedHashMap.get(word);
+			if (value == null)
+				value = 1;
+			else
+				value += 1;
+			myLinkedHashMap.add(word, value);
+		}
+		System.out.println(myLinkedHashMap);
+		int frequency = myLinkedHashMap.get("paranoid");
+		TestCase.assertEquals(3, frequency);
 	}
 }
